81a88f0ee7b3fdf7b03fdf79a54d1e72abddb498
Philippe F <phil.fremy@free.fr>
2024-02-08 23:29:28 +0100

    Avoid CI segfault

--- tests/test_switchbranchutils.py
+++ tests/test_switchbranchutils.py
@@ -32,7 +32,7 @@ class TestGitSwitchBranchUtils(unittest.TestCase):
             self.app = QApplication.instance()
             return
 
-        self.app = QApplication(['-platform', 'minimal'])
+        self.app = QApplication(['-platform', 'offscreen'])
 
     def tearDown(self):
         self.app = None
