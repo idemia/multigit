055b26cc46d4d5c3000a83f23b5e8c82373cff9d
Philippe Fremy <phil.fremy@free.fr>
2025-09-14 04:26:03 -0400

    Fix flatpak packaging

--- packaging/flatpak/build-flatpak.sh
+++ packaging/flatpak/build-flatpak.sh
@@ -1,4 +1,8 @@
-appstreamcli validate org.multigit.Multigit.metainfo.xml \
+scriptdir=$(dirname $(realpath $0))
+
+(cd $scriptdir && appstreamcli validate org.multigit.Multigit.metainfo.xml \
 	&& desktop-file-validate org.multigit.Multigit.desktop \
-	&& flatpak-builder --user --force-clean --install builddir org.multigit.Multigit.yaml && flatpak run org.multigit.Multigit --debug
+	&& cd $scriptdir/../.. \
+	&& flatpak-builder --user --force-clean --install builddir org.multigit.Multigit.yaml -v \
+	&& flatpak run org.multigit.Multigit --debug)
 	
