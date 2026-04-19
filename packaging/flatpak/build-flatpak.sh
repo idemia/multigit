4ac0fd1849d996fd038096b9529b59eff6c6c30b
Philippe F <phil.fremy@free.fr>
2026-04-19 08:24:40 +0200

    Supports out-of-directory build and use flathub linter

--- packaging/flatpak/build-flatpak.sh
+++ packaging/flatpak/build-flatpak.sh
@@ -1,15 +1,16 @@
 scriptdir=$(dirname $(realpath $0))
+basedir=$(realpath $(dirname $(realpath $0))/../../)
 
 app_id=io.github.idemia.Multigit
 
 
 (cd $scriptdir \
-    && echo "appstreamcli validate" && appstreamcli validate $app_id.metainfo.xml \
+    && flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream $app_id.metainfo.xml \
     && echo "desktop-file-validate" && desktop-file-validate $app_id.desktop \
-    && cd $scriptdir/../.. \
+    && cd $scriptdir/../../../multigit-flatpak-build \
     && echo "Building flatpak" \
-    && flatpak-builder --user --force-clean --install builddir $app_id.yaml -v \
+    && flatpak-builder --user --force-clean --install builddir $basedir/$app_id.yaml -v \
     && flatpak run $app_id --debug)
 
-    # && flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream $app_id.metainfo.xml \
+    # && echo "appstreamcli validate" && appstreamcli validate $app_id.metainfo.xml \
     #&& flatpak run --command=flathub-build org.flatpak.Builder --user --install builddir $app_id.yaml \
